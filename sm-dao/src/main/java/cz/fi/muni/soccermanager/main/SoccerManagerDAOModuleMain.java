f13eb4ffeed8d6dbb409e02624adbf85934d35d3
MichalRandak <michal.randak@gmail.com>
2018-10-16 15:54:21 +0200

    Added dummy main class to sm-dao project

---
+++ sm-dao/src/main/java/cz/fi/muni/soccermanager/main/SoccerManagerDAOModuleMain.java
@@ -0,0 +1,9 @@
+package cz.fi.muni.soccermanager.main;
+
+public class SoccerManagerDAOModuleMain {
+
+	public static void main(String[] args) {
+		System.out.println("SoccerManagerDAOModuleMain main");
+	}
+
+}
