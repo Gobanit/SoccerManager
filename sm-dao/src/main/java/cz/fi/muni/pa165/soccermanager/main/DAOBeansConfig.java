df5025e547ca6de98c53cf9ff263482c1b901429
MichalRandak <michal.randak@gmail.com>
2018-10-18 00:09:37 +0200

    wrote JavaDoc for DAOBeansConfig

--- sm-dao/src/main/java/cz/fi/muni/pa165/soccermanager/main/DAOBeansConfig.java
+++ sm-dao/src/main/java/cz/fi/muni/pa165/soccermanager/main/DAOBeansConfig.java
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * Class serves for spring to find DAO classes it will manage.
  * @author Michal Randak
  *
  */
-
 @Configuration
 @ComponentScan(basePackages = "cz.fi.muni.pa165.soccermanager.dao")
 public class DAOBeansConfig {
