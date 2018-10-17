e2f5bd43fabdd33ecbdacc38120d842b1eb5d717
MichalRandak <michal.randak@gmail.com>
2018-10-18 00:03:25 +0200

    refactored main and config class to make EntityManagerFactory managed by spring

--- sm-dao/src/main/java/cz/fi/muni/pa165/soccermanager/main/PersistenceBeansConfig.java
+++ sm-dao/src/main/java/cz/fi/muni/pa165/soccermanager/main/PersistenceBeansConfig.java
@@ -2,6 +2,8 @@ package cz.fi.muni.pa165.soccermanager.main;
 
 import java.sql.SQLException;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.sql.DataSource;
 
 import org.slf4j.Logger;
@@ -12,13 +14,24 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
-
+/**
+ * Class serves to config beans neccessary for using an in-memory database 
+ * 
+ * @author Michal Randak
+ *
+ */
 @Configuration
-public class InMemoryDatabaseSpring {
-    private static final Logger LOG = LoggerFactory.getLogger(InMemoryDatabaseSpring.class);
+public class PersistenceBeansConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(PersistenceBeansConfig.class);
 
+    /**
+     * Creates an instance of embedded H2 database using Spring-jdbc.
+     * @return instance of embedded database as DataSource
+     * @throws SQLException
+     * 
+     */
 	@Bean
-	public DataSource db() throws SQLException{
+	public DataSource dataSource() throws SQLException{
 		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
 		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.H2).build();
 		LOG.debug("Created Embedded DB");
@@ -26,5 +39,15 @@ public class InMemoryDatabaseSpring {
 		LOG.debug("Type: "+db.getConnection().getMetaData().getDatabaseProductName());
 		LOG.debug("Username: "+db.getConnection().getMetaData().getUserName());
 		return db;		
+	}
+	
+	/**
+	 * Creates an instance of EntityManagerFactory based on 'default' persistence unit
+	 * @return create entity manager factory instance
+	 * @throws SQLException
+	 */
+	@Bean(destroyMethod = "close")
+	public EntityManagerFactory entityManagerFactory() throws SQLException{
+		return Persistence.createEntityManagerFactory("default");
 	}
 }
