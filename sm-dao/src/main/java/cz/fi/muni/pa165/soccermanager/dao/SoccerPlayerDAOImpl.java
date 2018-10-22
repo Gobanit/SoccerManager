62762929306dee207542b869822a00947aa6e097
Dominik Pilar <445537@mail.muni.cz>
2018-10-22 07:54:56 +0200

    Repaired dao for players

--- sm-dao/src/main/java/cz/fi/muni/pa165/soccermanager/dao/SoccerPlayerDAOImpl.java
+++ sm-dao/src/main/java/cz/fi/muni/pa165/soccermanager/dao/SoccerPlayerDAOImpl.java
@@ -1,119 +1,40 @@
 package cz.fi.muni.pa165.soccermanager.dao;
 
 import cz.fi.muni.pa165.soccermanager.data.SoccerPlayer;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Named
+@Repository
 public class SoccerPlayerDAOImpl implements SoccerPlayerDAO {
-	private EntityManagerFactory emf;
 
-	@Inject
-	public SoccerPlayerDAOImpl(EntityManagerFactory emf) {
-		this.emf = emf;
-	}
+	@PersistenceContext
+	private EntityManager em;
 
 	@Override public List<SoccerPlayer> findAll() {
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = null;
-		try {
-			tx = em.getTransaction();
-			tx.begin();
-
-			List<SoccerPlayer> players = em.createQuery("from SoccerPlayer", SoccerPlayer.class).getResultList();
-			tx.commit();
-			return players;
-		}
-		catch (RuntimeException e) {
-			if ( tx != null && tx.isActive() ) tx.rollback();
-			throw e; // or display error message
-		}
-		finally {
-			em.close();
-		}
+		return em.createQuery("select sp from SoccerPlayer", SoccerPlayer.class).getResultList();
 	}
 
 	@Override public SoccerPlayer findById(Long id) {
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = null;
-		try {
-			tx = em.getTransaction();
-			tx.begin();
-
-			SoccerPlayer player = em.find(SoccerPlayer.class, id);
-			tx.commit();
-			return player;
-		}
-		catch (RuntimeException e) {
-			if ( tx != null && tx.isActive() ) tx.rollback();
-			throw e; // or display error message
-		}
-		finally {
-			em.close();
-		}
+		return em.find(SoccerPlayer.class, id);
 	}
 
-	@Override public SoccerPlayer create(SoccerPlayer player) {
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = null;
-		try {
-			tx = em.getTransaction();
-			tx.begin();
-
-			em.persist(player);
-			tx.commit();
-			return player;
-		}
-		catch (RuntimeException e) {
-			if ( tx != null && tx.isActive() ) tx.rollback();
-			throw e; // or display error message
-		}
-		finally {
-			em.close();
-		}
+	@Override public void create(SoccerPlayer player) {
+		em.persist(player);
 	}
 
-	@Override public SoccerPlayer update(SoccerPlayer player) {
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = null;
-		try {
-			tx = em.getTransaction();
-			tx.begin();
-
-			em.merge(player);
-			tx.commit();
-			return player;
-		}
-		catch (RuntimeException e) {
-			if ( tx != null && tx.isActive() ) tx.rollback();
-			throw e; // or display error message
-		}
-		finally {
-			em.close();
-		}
+	@Override public void update(SoccerPlayer player) {
+		em.merge(player);
 	}
 
 	@Override public void delete(SoccerPlayer player) {
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = null;
-		try {
-			tx = em.getTransaction();
-			tx.begin();
-
-			em.remove(player);
-			tx.commit();
-		}
-		catch (RuntimeException e) {
-			if ( tx != null && tx.isActive() ) tx.rollback();
-			throw e; // or display error message
-		}
-		finally {
-			em.close();
-		}
+		em.remove(player);
 	}
 }
