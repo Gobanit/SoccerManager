1faf3b0f68a67d71e4d23d5569a230f87b511c39
MichalRandak <michal.randak@gmail.com>
2018-10-23 16:36:18 +0200

    modified Team entity class by recommendations

--- sm-dao/src/main/java/cz/fi/muni/pa165/soccermanager/data/Team.java
+++ sm-dao/src/main/java/cz/fi/muni/pa165/soccermanager/data/Team.java
@@ -2,6 +2,7 @@ package cz.fi.muni.pa165.soccermanager.data;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -63,12 +64,75 @@ public class Team {
 	}
 
 	public List<SoccerPlayer> getPlayers() {
-		return players;
+		return Collections.unmodifiableList(players);
 	}
 
 	public void setPlayers(List<SoccerPlayer> players) {
 		this.players = players;
 	}
+	
+	public void addPlayer(SoccerPlayer p) {
+		getPlayers().add(p);
+		p.setTeam(this);
+	}
+	
+	public void removePlayer(SoccerPlayer p) {
+		getPlayers().remove(p);
+		p.setTeam(null);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((getChampionshipName() == null) ? 0 : getChampionshipName().hashCode());
+		result = prime * result + ((getClubName() == null) ? 0 : getClubName().hashCode());
+		result = prime * result + ((getCountry() == null) ? 0 : getCountry().hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Team)) {
+			return false;
+		}
+		Team other = (Team) obj;
+		if (getChampionshipName() == null) {
+			if (other.getChampionshipName() != null) {
+				return false;
+			}
+		} else if (!getChampionshipName().equals(other.getChampionshipName())) {
+			return false;
+		}
+		if (getClubName() == null) {
+			if (other.getClubName() != null) {
+				return false;
+			}
+		} else if (!getClubName().equals(other.getClubName())) {
+			return false;
+		}
+		if (getCountry() == null) {
+			if (other.getCountry() != null) {
+				return false;
+			}
+		} else if (!getCountry().equals(other.getCountry())) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Team [id=" + getId() + ", clubName=" + getClubName() + ", championshipName=" + getChampionshipName() + ", country="
+				+ getCountry() + ", players=" + getPlayers() + "]";
+	}
+	
 	
 	
 }
