82d063119773c481329c079ecc0661892bb02d96
Michal Mazourek <michal.mazourek@mail.muni.cz>
2018-10-23 17:57:47 +0200

    removed transactional annotations from MatchDAO

--- sm-dao/src/main/java/cz/fi/muni/pa165/soccermanager/dao/MatchDAOImpl.java
+++ sm-dao/src/main/java/cz/fi/muni/pa165/soccermanager/dao/MatchDAOImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 import cz.fi.muni.pa165.soccermanager.data.Match;
 
@@ -19,25 +18,21 @@ public class MatchDAOImpl implements MatchDAO {
     @PersistenceContext
     private EntityManager entityManager;
 	
-    @Transactional
     @Override
     public void save(Match match) {
     	entityManager.persist(match);
     }
 
-    @Transactional
     @Override
     public void delete(Match match) {
     	entityManager.remove(match);
     }
 
-    @Transactional
     @Override
     public Match findById(Long id) {
     	return entityManager.find(Match.class, id);
     }
 
-    @Transactional
     @Override
     public List<Match> findAll() {
     	return entityManager.createQuery("from Match", Match.class).getResultList();
