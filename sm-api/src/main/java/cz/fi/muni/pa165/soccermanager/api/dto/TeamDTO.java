d943a5a582fd89c0c56fc848761b65647e957908
MichalRandak <michal.randak@gmail.com>
2018-11-22 10:42:49 +0100

    added getters/setters in TeamDTO

--- sm-api/src/main/java/cz/fi/muni/pa165/soccermanager/api/dto/TeamDTO.java
+++ sm-api/src/main/java/cz/fi/muni/pa165/soccermanager/api/dto/TeamDTO.java
@@ -9,4 +9,35 @@ public class TeamDTO {
     private String championshipName;
     private String country;
     private BigDecimal budget;
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getClubName() {
+		return clubName;
+	}
+	public void setClubName(String clubName) {
+		this.clubName = clubName;
+	}
+	public String getChampionshipName() {
+		return championshipName;
+	}
+	public void setChampionshipName(String championshipName) {
+		this.championshipName = championshipName;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public BigDecimal getBudget() {
+		return budget;
+	}
+	public void setBudget(BigDecimal budget) {
+		this.budget = budget;
+	}
 }
