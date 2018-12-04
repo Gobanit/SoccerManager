0decfb6321f831bb2f20bef03eb125676aa88eaf
MichalRandak <michal.randak@gmail.com>
2018-12-05 00:50:17 +0100

    created basic matches page

--- sm-rest/src/main/webapp/angular_app.js
+++ sm-rest/src/main/webapp/angular_app.js
@@ -8,6 +8,7 @@ pa165soccerManagerApp.config(['$routeProvider',
         $routeProvider.
         when('/teams', {templateUrl: 'partials/manager.html', controller: 'TeamsCtrl'}).
         when('/teams/:teamId', {templateUrl: 'partials/team_detail.html', controller: 'TeamDetailCtrl'}).
+        when('/matches', {templateUrl: 'partials/matchesList.html', controller: 'MatchesCtrl'}).
         otherwise({redirectTo: '/teams'});
     }]);
 
@@ -50,3 +51,76 @@ soccerManagerControllers.controller('TeamDetailCtrl',
             console.log('AJAX loaded detail of product ' + $scope.product.name);
         });
     });
+
+soccerManagerControllers.controller('MatchesCtrl',
+    function ($scope, $routeParams, $rootScope, $http) {			
+		loadMatches($scope, $http);			
+		
+		//define method isAlreadyPlayed
+		$scope.alreadyPlayed = function (match) {
+			return alreadyPlayed(match);
+		};
+		//define method canBeSimulated
+		$scope.canBeSimulated = function (match) {
+			return canBeSimulated(match);
+		}
+		
+		//define method simulateMatch
+		$scope.simulateMatch = function(match) {
+			console.log('simulateMatch: matchId = '+match.id);
+		    $http.post(findHrefFromLinks(match.links, 'simulate')).then(
+				function success(response) {
+					console.log('simulated match ' + match.id + ' on server');
+
+					$rootScope.successAlert = 'Simulated match "'+match.id+'"';
+					$rootScope.errorAlert = null;
+					
+					//load new list of all products
+					loadMatches($scope, $http);
+				},
+				function error(response) {
+					console.log('server returned error');
+				    $rootScope.successAlert = null;
+					$rootScope.errorAlert = 'Cannot simulate match "'+match.id+'"!';
+				});
+			};
+});
+
+
+/* 
+ * Matches functions
+ */
+
+function loadMatches($scope, $http) {
+	$http.get('/pa165/matches/').then(function (response) {
+		console.log('AJAX response.data: ' + response.data);
+        $scope.matches = response.data.content;
+        console.log('AJAX loaded list of ' + $scope.matches.length + 'matches.');
+	});
+}
+
+function findHrefFromLinks(links, relName) {
+	var i;
+	for(i=0;i<links.length;i++) {
+		if(links[i].rel == relName) return links[i].href;
+	}
+	return null;
+}
+
+function alreadyPlayed(match) {
+	if(match.homeTeamGoals != null) return true;
+	if(match.awayTeamGoals != null) return true;
+	return false;
+}
+function canBeSimulated(match) {
+	if(alreadyPlayed(match)) return false;
+	
+	var date = Date.parse(match.date);
+	var now = new Date();
+	
+	if(now < date) return false;
+	
+	return true;
+}
+
+
